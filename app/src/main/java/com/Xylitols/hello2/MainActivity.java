5d224ce1127db99acfbe81df39e3b523f50ce06c
Xylitols <64079134+Xylitols@users.noreply.github.com>
2020-04-28 11:27:37 +0900

    94page

--- app/src/main/java/com/Xylitols/hello2/MainActivity.java
+++ app/src/main/java/com/Xylitols/hello2/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {  //  MainActivity class 는 AppCompatActivity 라는 부모 Class에서 상속받았다는 의미
 
     // 버튼이 클릭된 횟수를 저장하는 변수를 선언한다.
     int clickCount = 0;
@@ -14,10 +14,11 @@ public class MainActivity extends AppCompatActivity {
     /**
      * MainActivity 가 최초 실행될때 시작한다.
      * 자바독 주석을 확인하려면 F1 키를 누르자.
+     *
      * @param savedInstanceState
      */
 
-    @Override
+    @Override  //override : 무시하다, 무효하다, 기각하다. 부모 Class에서 정의한 메서드를 자식 Class에서 변경하는 것
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
@@ -33,10 +34,18 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 // 클릭 카운트를 1회 증가시킨다.
-                clickCount = clickCount +1;
+                clickCount = clickCount + 1;
 
-                // 버튼이 클릭되면 클릭된 횟수를 증가하면서 토스트 메세지를 보여준다.
-                Toast.makeText(getApplicationContext(), "clickCount: " + clickCount, Toast.LENGTH_SHORT).show();
+                if (clickCount % 2 == 0) {
+                    // 클릭 카운트가 2의 배수이면 버튼이 클릭된 횟수를 잠시 보여준다.
+                    Toast.makeText(getApplicationContext(), "clickCount: " + clickCount, Toast.LENGTH_SHORT).show();
+                } else if (clickCount % 3 == 0) {
+                    // 클릭 카운트가 3의 배수이면 Hello, World 메시지를 잠시 보여준다.
+                    Toast.makeText(getApplicationContext(), "Hello, World", Toast.LENGTH_SHORT).show();
+                } else {
+                    // 클릭 카운트가 2의 배수도 아니고 3의 배수도 아닌 경우 "Hello" 메세지를 잠시 보여준다.
+                    Toast.makeText(getApplicationContext(), "Hello", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
